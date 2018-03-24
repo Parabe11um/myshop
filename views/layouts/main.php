4929d93289b719ab0cdd5f66ac1cd7e1062ab749
rombweb <r.a.bazhenoff@gmail.com>
2018-03-24 17:38:51 +0300

    24/03/2018 update

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -44,8 +44,6 @@ $this->beginPage() ?>
                     <ul class="main_menu">
                         <li><a href="/">Главная</a></li>
                         <li><a href="<?= \yii\helpers\Url::to(['/catalog']) ?>">Каталог</a></li>
-                        <li><a href="<?= \yii\helpers\Url::to('about') ?>">О нас</a></li>
-                        <li><a href="contact.html">Контакты</a></li>
                     </ul>
                 </nav>
             </div>
@@ -57,74 +55,10 @@ $this->beginPage() ?>
                 <div class="header-wrapicon2">
                     <img src="/images/icons/icon-header-02.png" onclick="return getCart()" class="header-icon1" alt="ICON">
                     <span class="header-icons-noti">0</span>
-
-                    <!-- Header cart noti -->
-                    <div class="header-cart header-dropdown">
-                        <ul class="header-cart-wrapitem">
-                            <li class="header-cart-item">
-                                <div class="header-cart-item-img">
-                                    <img src="/images/item-cart-01.jpg" alt="IMG">
-                                </div>
-                                <div class="header-cart-item-txt">
-                                    <a href="#" class="header-cart-item-name">White Shirt With Pleat Detail Back</a>
-                                    <span class="header-cart-item-info">1 x $19.00</span>
-                                </div>
-                            </li>
-
-                            <li class="header-cart-item">
-                                <div class="header-cart-item-img">
-                                    <img src="/images/item-cart-02.jpg" alt="IMG">
-                                </div>
-
-                                <div class="header-cart-item-txt">
-                                    <a href="#" class="header-cart-item-name">Converse All Star Hi Black Canvas</a>
-
-                                    <span class="header-cart-item-info">1 x $39.00</span>
-                                </div>
-                            </li>
-
-                            <li class="header-cart-item">
-                                <div class="header-cart-item-img">
-                                    <img src="/images/item-cart-03.jpg" alt="IMG">
-                                </div>
-
-                                <div class="header-cart-item-txt">
-                                    <a href="#" class="header-cart-item-name">
-                                        Nixon Porter Leather Watch In Tan
-                                    </a>
-
-                                    <span class="header-cart-item-info">
-											1 x $17.00
-										</span>
-                                </div>
-                            </li>
-                        </ul>
-
-                        <div class="header-cart-total">
-                            Total: $75.00
-                        </div>
-
-                        <div class="header-cart-buttons">
-                            <div class="header-cart-wrapbtn">
-                                <!-- Button -->
-                                <a href="cart.html" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">
-                                    View Cart
-                                </a>
-                            </div>
-
-                            <div class="header-cart-wrapbtn">
-                                <!-- Button -->
-                                <a href="#" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">
-                                    Check Out
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+               </div>
             </div>
         </div>
     </div>
-
     <!-- Header Mobile -->
     <div class="wrap_header_mobile">
         <!-- Logo moblie -->
@@ -228,80 +162,21 @@ $this->beginPage() ?>
             </div>
         </div>
     </div>
-
     <!-- Menu Mobile -->
     <div class="wrap-side-menu" >
         <nav class="side-menu">
             <ul class="main-menu">
-                <li class="item-topbar-mobile p-l-20 p-t-8 p-b-8">
-						<span class="topbar-child1">
-							Free shipping for standard order over $100
-						</span>
-                </li>
-
-                <li class="item-topbar-mobile p-l-20 p-t-8 p-b-8">
-                    <div class="topbar-child2-mobile">
-							<span class="topbar-email">
-								fashe@example.com
-							</span>
-
-                        <div class="topbar-language rs1-select2">
-                            <select class="selection-1" name="time">
-                                <option>USD</option>
-                                <option>EUR</option>
-                            </select>
-                        </div>
-                    </div>
-                </li>
-
-                <li class="item-topbar-mobile p-l-10">
-                    <div class="topbar-social-mobile">
-                        <a href="#" class="topbar-social-item fa fa-facebook"></a>
-                        <a href="#" class="topbar-social-item fa fa-instagram"></a>
-                        <a href="#" class="topbar-social-item fa fa-pinterest-p"></a>
-                        <a href="#" class="topbar-social-item fa fa-snapchat-ghost"></a>
-                        <a href="#" class="topbar-social-item fa fa-youtube-play"></a>
-                    </div>
+                <li class="item-menu-mobile">
+                    <a href="/">Главная</a>
                 </li>
 
                 <li class="item-menu-mobile">
-                    <a href="index.html">Home</a>
-                    <ul class="sub-menu">
-                        <li><a href="index.html">Homepage V1</a></li>
-                        <li><a href="home-02.html">Homepage V2</a></li>
-                        <li><a href="home-03.html">Homepage V3</a></li>
-                    </ul>
-                    <i class="arrow-main-menu fa fa-angle-right" aria-hidden="true"></i>
-                </li>
-
-                <li class="item-menu-mobile">
-                    <a href="product.html">Shop</a>
-                </li>
-
-                <li class="item-menu-mobile">
-                    <a href="product.html">Sale</a>
-                </li>
-
-                <li class="item-menu-mobile">
-                    <a href="cart.html">Features</a>
-                </li>
-
-                <li class="item-menu-mobile">
-                    <a href="blog.html">Blog</a>
-                </li>
-
-                <li class="item-menu-mobile">
-                    <a href="about.html">About</a>
-                </li>
-
-                <li class="item-menu-mobile">
-                    <a href="contact.html">Contact</a>
+                    <a href="<?= \yii\helpers\Url::to(['/catalog']) ?>">Каталог</a>
                 </li>
             </ul>
         </nav>
     </div>
 </header>
-
 
 <?= $content; ?>
 
@@ -312,12 +187,10 @@ $this->beginPage() ?>
             <h4 class="s-text12 p-b-30">
                 GET IN TOUCH
             </h4>
-
             <div>
                 <p class="s-text7 w-size27">
                     Any questions? Let us know in store at 8th floor, 379 Hudson St, New York, NY 10018 or call us on (+1) 96 716 6879
                 </p>
-
                 <div class="flex-m p-t-30">
                     <a href="#" class="fs-18 color1 p-r-20 fa fa-facebook"></a>
                     <a href="#" class="fs-18 color1 p-r-20 fa fa-instagram"></a>
@@ -327,12 +200,10 @@ $this->beginPage() ?>
                 </div>
             </div>
         </div>
-
         <div class="w-size7 p-t-30 p-l-15 p-r-15 respon4">
             <h4 class="s-text12 p-b-30">
                 Categories
             </h4>
-
             <ul>
                 <li class="p-b-9">
                     <a href="#" class="s-text7">
@@ -359,7 +230,6 @@ $this->beginPage() ?>
                 </li>
             </ul>
         </div>
-
         <div class="w-size7 p-t-30 p-l-15 p-r-15 respon4">
             <h4 class="s-text12 p-b-30">
                 Links
@@ -391,12 +261,10 @@ $this->beginPage() ?>
                 </li>
             </ul>
         </div>
-
         <div class="w-size7 p-t-30 p-l-15 p-r-15 respon4">
             <h4 class="s-text12 p-b-30">
                 Help
             </h4>
-
             <ul>
                 <li class="p-b-9">
                     <a href="#" class="s-text7">
@@ -423,12 +291,10 @@ $this->beginPage() ?>
                 </li>
             </ul>
         </div>
-
         <div class="w-size8 p-t-30 p-l-15 p-r-15 respon3">
             <h4 class="s-text12 p-b-30">
                 Newsletter
             </h4>
-
             <form>
                 <div class="effect1 w-size9">
                     <input class="s-text7 bg6 w-full p-b-5" type="text" name="email" placeholder="email@example.com">
@@ -445,36 +311,7 @@ $this->beginPage() ?>
             </form>
         </div>
     </div>
-
-    <div class="t-center p-l-15 p-r-15">
-        <a href="#">
-            <img class="h-size2" src="/images/icons/paypal.png" alt="IMG-PAYPAL">
-        </a>
-
-        <a href="#">
-            <img class="h-size2" src="/images/icons/visa.png" alt="IMG-VISA">
-        </a>
-
-        <a href="#">
-            <img class="h-size2" src="/images/icons/mastercard.png" alt="IMG-MASTERCARD">
-        </a>
-
-        <a href="#">
-            <img class="h-size2" src="/images/icons/express.png" alt="IMG-EXPRESS">
-        </a>
-
-        <a href="#">
-            <img class="h-size2" src="/images/icons/discover.png" alt="IMG-DISCOVER">
-        </a>
-
-        <div class="t-center s-text8 p-t-20">
-            Copyright © 2018 All rights reserved. | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-        </div>
-    </div>
 </footer>
-
-
-
 
 <!-- Back to top -->
 <div class="btn-back-to-top bg0-hov" id="myBtn">
@@ -482,8 +319,7 @@ $this->beginPage() ?>
 			<i class="fa fa-angle-double-up" aria-hidden="true"></i>
 		</span>
 </div>
-
-<!-- Container Selection1 -->
+election1 -->
 <div id="dropDownSelect1"></div>
 
 <!--===============================================================================================-->
